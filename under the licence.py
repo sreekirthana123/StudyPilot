66352498167b4079b662b5a4628672b1adcc3c08
V Sree Kirthana <sreekeerthana64@gmail.com>
2026-06-20 22:47:17 +0530

    Add files via upload

---
+++ under the licence.py
@@ -0,0 +1,23 @@
+Copyright (c) 2026 V Sree Kirthana. All Rights Reserved.
+
+This source code is the intellectual property of the author. Permission is granted to view this repository and its contents for educational and reference purposes only.
+
+Strictly Prohibited:
+
+Copying or reproducing the code
+
+Modifying or creating derivative works
+
+Re-uploading or redistributing the code
+
+Using the code for personal, academic, commercial, or research purposes
+
+Claiming ownership of any part of this project
+
+Using project ideas, structure, or implementation without explicit written permission
+
+No part of this repository may be used, shared, published, or modified in any form.
+
+Unauthorized use, reproduction, or distribution of this code may result in legal action.
+
+For permissions, contact the author directly.
